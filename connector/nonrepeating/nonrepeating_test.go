f5e44ab7bbdfe53ebb624e8a2a55db02c9e5c067
1710097899 <1710097899@qq.com>
2022-06-23 15:34:10 +0800

    change

--- connector/nonrepeating/nonrepeating_test.go
+++ connector/nonrepeating/nonrepeating_test.go
@@ -20,7 +20,12 @@ func TestSubstr(t *testing.T) {
 
 func BenchmarkSubstr(b *testing.B) {
 	s := "asddsadkaksk"
+	for i := 0; i < 20; i++ {
+		s = s + s
+	}
 	ans := 4
+	b.Logf("len(s) %d", len(s))
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		asdjk := lengthOfNunRepeatingSubStr(s)
 		if asdjk != ans {
